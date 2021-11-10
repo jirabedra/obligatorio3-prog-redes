883e36cf7e5db17e2c879bf4472df049d75c13ab
Juan Irabedra <juaniirabedra@gmail.com>
2021-11-10 12:39:43 -0300

    agrego API para adminserver

---
+++ Obligatorio/AdminServer/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using AdminServer.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminServer.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [Route("games")]
+    [ApiController]
+    public class GameController : Controller
+    {
+        private static GameService gameService = new GameService();
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> Get(string name)
+        {
+            var res = Task.Run(async () => await gameService.GetGame(name)).Result;
+            return Ok(res);
+        }
+    }
+}
---
+++ Obligatorio/AdminServer/Services/GameService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminServer.Services
+{
+    public class GameService
+    {
+        internal async Task<string> GetGame(string name)
+        {
+            var ret = Task.Run(async () =>
+            {
+                string dummy = await Dummy();
+                return dummy;
+            });
+            return ret.Result;
+        }
+
+        private async Task<string> Dummy()
+        {
+            return "hola";
+        }
+    }
+
+
+}
