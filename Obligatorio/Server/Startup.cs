f55eb31042a485a279a82f8abaa0da49743cde5a
Juan Irabedra <juaniirabedra@gmail.com>
2021-11-11 15:02:31 -0300

    algun que otro .proto hecho

--- Obligatorio/Server/Startup.cs
+++ Obligatorio/Server/Startup.cs
@@ -31,7 +31,7 @@ namespace Server
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();
 
                 endpoints.MapGet("/", async context =>
                 {
